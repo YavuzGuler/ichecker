204567846eb60a09c1b9e49ff18e22e6cb01cafd
YavuzGuler <yavuzguler.cs@gmail.com>
2020-12-13 12:33:11 +0300

    merge

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
     HashOperations sa=new HashOperations();
-       System.out.println( sa.byteArrayToHexDecimal(sa.SHA256("sas")));
         PPKey ppKey=new PPKey();
         ppKey.createKeyPair("prikey","cert");
     }
