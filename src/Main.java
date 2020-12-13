2d311a58424a62d518d94a83507c85ff4edba5c7
YavuzGuler <yavuzguler.cs@gmail.com>
2020-12-13 14:59:45 +0300

    -

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,6 @@ public class Main {
 
     HashOperations sa=new HashOperations();
         PPKey ppKey=new PPKey();
-        //ppKey.createKeyPair("prikey","cert");
-        byte [] sign=ppKey.encrypt("selamin aleyküm".getBytes(StandardCharsets.UTF_8),ppKey.readCertificate("cert"));
-        System.out.println(new String(sign));
-        sign=ppKey.decryption(sign,ppKey.readPrivateKey("prikey"));
-        System.out.println(new String(sign));
-
-        //ppKey.createKeyPair("prikey","cert");
-       // readpathandhashinside();
 
     }
 
